c67b936a7f2966fb91de52d02db2807000ecfccc
totojanin2 <totojanin2@gmail.com>
2021-07-31 23:34:45 -0300

    advances in owner onboarding (without schema validation and failed tests)

--- src/app/controllers/users.js
+++ src/app/controllers/users.js
@@ -1,8 +1,13 @@
 const { createUserSerializer } = require('../serializers/users');
 const { createUser, updateUser } = require('../services/users');
-const { createUserMapper, onBoardingWalkerMapper } = require('../mappers/users');
+const {
+  createUserMapper,
+  onBoardingWalkerMapper,
+  onBoardingOwnerMapper,
+} = require('../mappers/users');
 const { createAddress } = require('../services/addresses');
-const { bulkCreateRanges } = require('../services/range');
+const { bulkCreateRanges } = require('../services/ranges');
+const { bulkCreatePets } = require('../services/pets');
 const { sequelizeInstance: sequelize } = require('../models');
 const logger = require('../logger');
 
@@ -30,6 +35,30 @@ exports.onBoardingWalker = async (req, res, next) => {
     res.status(200).json(updatedUser);
   } catch (error) {
     logger.error('Error onboarding WALKER:', error);
+    transaction.rollback();
+    res.status(500).json(error);
+  }
+};
+
+exports.onBoardingOwner = async (req, res, next) => {
+  const transaction = await sequelize.transaction();
+  try {
+    const { id } = req.params;
+    const { address: addressData, pets, ...restData } = onBoardingOwnerMapper(req);
+    const address = await createAddress({ data: { ...addressData }, options: { transaction } });
+
+    const updatedUser = await updateUser({
+      id,
+      data: { addressId: address.get('id'), ...restData },
+      options: { transaction },
+    });
+
+    await bulkCreatePets({ pets, ownerId: updatedUser.id, options: { transaction } });
+
+    transaction.commit();
+    res.status(200).json('User updated successfully');
+  } catch (error) {
+    logger.error('Error onboarding WALKER:' + error);
     transaction.rollback();
     res.status(500).json(error);
   }
