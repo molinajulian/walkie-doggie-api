4525a55942f9afa56a969f054b85baefc109a710
Julian <molina.bohemio@gmail.com>
2021-08-29 00:07:37 -0300

    a

--- src/app/controllers/health_check.js
+++ src/app/controllers/health_check.js
@@ -3,6 +3,6 @@ const { redis } = require('../../config');
 
 exports.healthCheck = async (_, res) => {
   const queue = await new Queue('work', redis.url);
-  await queue().add();
+  await queue.add();
   return res.status(200).send({ uptime: process.uptime() });
 };
