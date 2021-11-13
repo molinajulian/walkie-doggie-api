57b820c13f1a7babc9fd4b4321e0c9d4f27a1535
Julian <molina.bohemio@gmail.com>
2021-11-13 19:23:24 -0300

    add petWalks router

--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -3,9 +3,10 @@ const usersRouter = require('./users');
 const sessionsRouter = require('./sessions');
 const centersRouter = require('./centers');
 const complaintsRouter = require('./complaints');
+const petWalksRouter = require('./pet_walks');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  const routes = [usersRouter, sessionsRouter, centersRouter, complaintsRouter];
+  const routes = [usersRouter, sessionsRouter, centersRouter, complaintsRouter, petWalksRouter];
   routes.forEach(route => route.init(app));
 };
